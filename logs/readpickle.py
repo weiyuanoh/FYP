483c64e2e26cfc8865464aefe12640683b22c6f4
weiyuanoh <113036339+weiyuanoh@users.noreply.github.com>
2025-02-21 11:38:36 +0800

    Multi processing of MCMC

--- logs/readpickle.py
+++ logs/readpickle.py
@@ -1,6 +1,6 @@
 import pickle 
 
-filename = r"FYP\logs\results_20250220_183435_beta=0.65-0.15_iter=5000_burn=2000_sigma=0.01_points=200_ab93e248.pkl"  # Replace with your actual file name
+filename = r"FYP\logs\results_20250221_112433_beta_true=0.65-0.15_iter=10000_burn=5000_sigma=0.01_points=100_cde886ab.pkl"  # Replace with your actual file name
 with open(filename, 'rb') as f:
     data = pickle.load(f)
 
